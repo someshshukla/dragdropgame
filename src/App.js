d3b2b8603fb3b58d6c226f1c20ed35760e075434
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-17 21:20:43 +0530

    css, tile and card functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import Tile from './components/Tile';
+import Card from './components/Card';
+
+
 
 function App() {
   return (
     <div className="App">
       <main className="flexbox">  
-        cards
+        <Tile id="tile-1" className="tile">
+          <Card id="card-1" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+        </Tile>
+
+        <Tile id="tile-2" className="tile">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card one</p>
+          </Card>
+        </Tile>
       </main>
     </div>
   );
