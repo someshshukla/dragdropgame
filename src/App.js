23c8917cb3fedc7d1c02bdb9761dda4044f3e92c
Somesh Shukla <somesh13shukla@gmail.com>
2023-02-18 12:52:48 +0530

    made the css lavender and added more tiles and card

--- src/App.js
+++ src/App.js
@@ -8,17 +8,56 @@ function App() {
   return (
     <div className="App">
       <main className="flexbox">  
-        <Tile id="tile-1" className="tile">
+        <Tile id="tile1" className="tile1">
           <Card id="card-1" className="card" draggable="true">
-            <p>Card one</p>
+            <p>Card1</p>
           </Card>
         </Tile>
 
-        <Tile id="tile-2" className="tile">
+        <Tile id="tile2" className="tile2">
           <Card id="card-2" className="card" draggable="true">
-            <p>Card one</p>
+            <p>Card2</p>
           </Card>
         </Tile>
+
+        <Tile id="tile3" className="tile3">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card3</p>
+          </Card>
+        </Tile>
+
+        <Tile id="tile4" className="tile4">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card4</p>
+          </Card>
+        </Tile>
+
+        <Tile id="tile5" className="tile5">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card5</p>
+          </Card>
+        </Tile>
+
+        {/* <Tile id="tile6" className="tile6">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card6</p>
+          </Card>
+        </Tile>
+
+        <Tile id="tile7" className="tile7">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card7</p>
+          </Card>
+        </Tile>
+
+        <Tile id="tile8" className="tile8">
+          <Card id="card-2" className="card" draggable="true">
+            <p>Card8</p>
+          </Card>
+        </Tile>
+ */}
+
+
       </main>
     </div>
   );
